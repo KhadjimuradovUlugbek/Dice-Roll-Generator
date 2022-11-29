34f941e9fc286ccd83ac6e0c8569ac8a733b2166
Khadjimuratov Ulugbek <khadjimuratovulugbek@gmail.com>
2022-11-29 13:16:19 +0000

    This is the done whole project

--- dice_roll.py
+++ dice_roll.py
@@ -5,3 +5,26 @@ import random
 min_val = 1
 
 max_val = 10
+
+#The variable that stores the user's decision
+
+roll_again = "yes"
+
+#The dice rool loop if the user wants to continue
+
+while roll_again == "yes" or roll_again == "y":
+    
+    print("Dice rolling...")
+    print("The values are: ")
+    
+    #Printing the randomly generated variable of the first dice
+    
+    print(random.randint(min_val, max_val))
+    
+    #Printing the randomly generated variable of the second dice 
+    
+    print(random.randint(min_val, max_val))
+    
+    #Here the user enters yes or y to continue and other input ends the programm
+    
+    roll_again = input("Please write 'yes' or 'y' to roll the dice again: ")
